93c5ab0eebd4a9b1282f9d650da7b4dcd7f0bcf5
Jakub Tomaszewski <tomaszewski.jra@gmail.com>
2022-03-21 13:20:09 +0100

    TODO resolved + testing module

---
+++ code/simplicial_complex_test.py
@@ -0,0 +1,19 @@
+import unittest
+from simplicial_complex import ChainComplex
+
+class TestComplexMethods(unittest.TestCase):
+
+    def test_simplex_addition(self, test_object):
+        test_object = ChainComplex
+        test_object.add_simplex([0,1])
+        self.assertTrue({0,1} in test_object[2].keys())
+
+    def test_simplex_deletion(self, test_object):
+        test_object = ChainComplex
+        test_object.add_simplex([0,1])
+        test_object.delete_simplex({0,1})
+        self.assertTrue({0,1} not in test_object[2].keys())
+
+
+if __name__ == '__main__':
+    unittest.main()
